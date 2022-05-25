0fee3e47a77ba5953c34fb5af667c84b92feb3f9
Ricardo Salinas  Marin <100739835+Richysm99@users.noreply.github.com>
2022-05-24 20:32:27 -0500

    Create Parcial3.py

---
+++ Parcial3.py
@@ -0,0 +1,20 @@
+
+# Dado el diagrama, escriba la representación en código del mismo.
+
+class Persona:
+    def __init__(self,nombre,nif,fechaNac):
+        self.nombre = Nombre
+        self.nif = Nif
+        self.fechaNac = Fecha
+
+        def CrearPersona(self):
+            return "Crear Persona"
+            
+----------------------------------------------------------------------------------
+
+class Jugador(Persona):
+    def __init__(self,numFed):
+        self.numFed = integer
+        
+    def CrearJugador(self):
+            return "Crear Jugador"   
